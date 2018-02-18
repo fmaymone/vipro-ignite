a376c53dea835e944e4690556e6999ba744149ee
Fernando Maymone <fmaymone@gmail.com>
2018-02-18 10:25:41 -0300

    First Commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import ProcessosView from '../Containers/ProcessosView'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  ProcessosView: { screen: ProcessosView },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'ProcessosView',
   navigationOptions: {
     headerStyle: styles.header
   }
